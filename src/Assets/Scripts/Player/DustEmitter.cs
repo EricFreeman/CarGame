32e1f65efae6093c026c85216c41d8057010c405
Eric Freeman <clownhunter@gmail.com>
2016-04-26 16:32:49 -0600

    Added dust particles.

---
+++ src/Assets/Scripts/Player/DustEmitter.cs
@@ -0,0 +1,39 @@
+﻿using Assets.Scripts.Environment;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class DustEmitter : MonoBehaviour
+    {
+        public Dust DustGameObject;
+        public float SpawnDistance = .2f;
+
+        private Vector3 _lastPosition;
+
+        void Start()
+        {
+            _lastPosition = transform.position;
+        }
+
+        void Update()
+        {
+            var delta = transform.position - _lastPosition;
+            var particlesToSpawn = (int)(delta.magnitude/SpawnDistance);
+
+            Vector3 spawnPosition = _lastPosition;
+            for (var i = 0; i < particlesToSpawn; i++)
+            {
+                spawnPosition = Vector3.MoveTowards(spawnPosition, transform.position, SpawnDistance);
+                SpawnDust(spawnPosition);
+                _lastPosition = transform.position;
+            }
+
+        }
+
+        private void SpawnDust(Vector3 position)
+        {
+            var dust = Instantiate(DustGameObject);
+            dust.transform.position = position;
+        }
+    }
+}
