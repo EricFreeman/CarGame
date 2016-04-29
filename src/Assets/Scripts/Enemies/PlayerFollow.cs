0781529fb44f346efaeb2a1ab96a8f1fb01c21d2
Eric Freeman <clownhunter@gmail.com>
2016-04-29 17:40:51 -0600

    Added offset that changes around player that the enemies chase so they don't just clump up in a swarm around the player.

--- src/Assets/Scripts/Enemies/PlayerFollow.cs
+++ src/Assets/Scripts/Enemies/PlayerFollow.cs
@@ -5,10 +5,28 @@ namespace Assets.Scripts.Enemies
     public class PlayerFollow : MonoBehaviour
     {
         public GameObject Player;
+        public float MaxChangeOffsetTime = 5f;
+        public float MaxOffsetAmount = 10f;
+
+        private Vector3 _offset;
+        private float _currentChangeOffsetTime;
+
+        void Start()
+        {
+            _currentChangeOffsetTime = Random.Range(1, MaxChangeOffsetTime);
+        }
 
         void Update()
         {
-            GetComponent<NavMeshAgent>().destination = Player.transform.position;
+            _currentChangeOffsetTime -= Time.deltaTime;
+
+            if (_currentChangeOffsetTime <= 0)
+            {
+                _currentChangeOffsetTime = Random.Range(1, MaxChangeOffsetTime);
+                _offset = new Vector3(Random.Range(-MaxOffsetAmount, MaxOffsetAmount), 0, Random.Range(-MaxOffsetAmount, MaxOffsetAmount));
+            }
+
+            GetComponent<NavMeshAgent>().destination = Player.transform.position + _offset;
         }
     }
 }
