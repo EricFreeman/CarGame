b2d0ca094e1a5a65e63e6179c45154cffbbe6dce
Eric Freeman <clownhunter@gmail.com>
2016-05-02 16:33:42 -0600

    Added images for player and enemies.

---
+++ src/Assets/Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerCollision : MonoBehaviour
+    {
+        void OnCollisionEnter(Collision collider)
+        {
+            var rigidBody = GetComponent<Rigidbody>();
+            rigidBody.AddForce(collider.relativeVelocity * 100);
+            Debug.Log("shit");
+        }
+    }
+}
