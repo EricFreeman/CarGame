0c291517000d5528da8d21c2f4de2e846bf0c051
Eric Freeman <clownhunter@gmail.com>
2016-05-30 18:02:32 -0600

    Added MinSize to Resizable component.

--- src/Assets/Scripts/UI/Resizable.cs
+++ src/Assets/Scripts/UI/Resizable.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts.UI
     public class Resizable : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
         public RectTransform Target;
+        public Vector2 MinSize;
 
         private bool _isMouseDown;
         private Vector3 _lastPosition;
@@ -30,6 +31,17 @@ namespace Assets.Scripts.UI
                 _lastPosition = currentPosition;
 
                 Target.sizeDelta = new Vector2(Target.sizeDelta.x + diff.x, Target.sizeDelta.y - diff.y);
+
+                if (Target.sizeDelta.x < MinSize.x)
+                {
+                    Target.sizeDelta = new Vector2(MinSize.x, Target.sizeDelta.y);
+                }
+
+                if (Target.sizeDelta.y < MinSize.y)
+                {
+                    Target.sizeDelta = new Vector2(Target.sizeDelta.x, MinSize.y);
+                }
+
             }
         }
     }
