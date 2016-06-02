795a2b2a9416b866a7881cc8d310d5fbdf3629d2
Eric Freeman <clownhunter@gmail.com>
2016-06-01 19:06:40 -0600

    Made it so it rotates the front wheels of your car when turning.

---
+++ src/Assets/Scripts/Player/PlayerWheels.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class PlayerWheels : MonoBehaviour
+    {
+        public float MaxRotation = 30f;
+        public float TurnSpeed = 100f;
+
+        void Update()
+        {
+            var turnAmount = Input.GetAxisRaw("Horizontal");
+
+            if (turnAmount < 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, Mathf.MoveTowardsAngle(transform.localEulerAngles.y, -MaxRotation, TurnSpeed * Time.deltaTime), 0);
+            }
+            else if (turnAmount > 0)
+            {
+                transform.localRotation = Quaternion.Euler(0, Mathf.MoveTowardsAngle(transform.localEulerAngles.y, MaxRotation, TurnSpeed * Time.deltaTime), 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, Mathf.MoveTowardsAngle(transform.localEulerAngles.y, 0, TurnSpeed * Time.deltaTime), 0);
+            }
+        }
+    }
+}
