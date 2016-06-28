b5c58b347aa5cb41702feb78acd50358e7d1076e
Eric Freeman <clownhunter@gmail.com>
2016-06-27 22:16:02 -0600

    Move camera better so player can see what's coming up.

--- src/Assets/Scripts/Camera/CameraTrack.cs
+++ src/Assets/Scripts/Camera/CameraTrack.cs
@@ -6,20 +6,28 @@ namespace Assets.Scripts.Camera
     {
         public GameObject GameObject;
         public Vector3 Offset;
-        public float SmoothTime = 0.3f;
 
+        public float SmoothTime = 0.3f; 
         private Vector3 _velocity = Vector3.zero;
+        private Vector3 _lastPosition;
 
         void Start()
         {
             transform.position = GameObject.transform.position + Offset;
+            _lastPosition = GameObject.transform.position;
         }
 
-        void Update()
+        void LateUpdate()
         {
-            var goalPos = GameObject.transform.position;
+            var currentPosition = GameObject.transform.position;
+            var delta = currentPosition - _lastPosition;
+
+            var goalPos = GameObject.transform.position + (delta * 30f);
             goalPos.y = transform.position.y;
+            
             transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref _velocity, SmoothTime);
+
+            _lastPosition = GameObject.transform.position;
         }
     }
 }
