be7ec562a9cf0ced8f401118e7649127011e3a8d
Eric Freeman <clownhunter@gmail.com>
2016-07-21 23:06:40 -0600

    Added boost to back of car when you first start boosting.

---
+++ src/Assets/Scripts/Player/Boost.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Boost : MonoBehaviour
+    {
+        public GameObject BoostGameObject;
+
+        private float _previousVertical;
+
+        void Update()
+        {
+            var currentVertical = Input.GetAxisRaw("Vertical");
+
+            if (_previousVertical < .1f && currentVertical > .1f)
+            {
+                var boost = Instantiate(BoostGameObject);
+                boost.transform.position = transform.position;
+                boost.transform.rotation = transform.rotation;
+                boost.transform.SetParent(transform);
+            }
+
+            _previousVertical = currentVertical;
+        }
+    }
+}
