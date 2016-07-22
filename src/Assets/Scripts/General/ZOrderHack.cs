16f1888c2257d737bdd13567b68b878208422d10
Eric Freeman <clownhunter@gmail.com>
2016-07-21 21:55:59 -0600

    Fixed crater z-order bullshit

---
+++ src/Assets/Scripts/General/ZOrderHack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.General
+{
+    public class ZOrderHack : MonoBehaviour
+    {
+        void Start()
+        {
+            var nextLayer = StaticBullshitHack.GetNextCraterSortingOrder();
+            Debug.Log(nextLayer);
+            GetComponentInChildren<SpriteRenderer>().sortingOrder = nextLayer;
+        }
+    }
+
+    public static class StaticBullshitHack
+    {
+        private static int _bullshit;
+
+        public static int GetNextCraterSortingOrder()
+        {
+            return _bullshit++;
+        }
+    }
+}
